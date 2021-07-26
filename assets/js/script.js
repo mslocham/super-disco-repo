762df69e0a50ccb3185bc14a59cd063ad16cfc36
Gugnu <lochammehtab@gmail.com>
2021-07-25 22:02:42 -0400

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+
+var today = moment().format('dddd');
+var currentTime = moment().format('h');
+
+var officeHrs = [9, 10, 12, 13, 14, 15, 16, 17];
+
+
