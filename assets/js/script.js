b6d7b3c3c96a2150ff67fde399e0898704564e83
Gugnu <lochammehtab@gmail.com>
2021-07-25 22:38:44 -0400

    initial changes made

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,19 @@ $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 var today = moment().format('dddd');
 var currentTime = moment().format('h');
 
-var officeHrs = [9, 10, 12, 13, 14, 15, 16, 17];
+var officeHrs = ["9AM", "10AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+
+for (let i = 0; i < officeHrs.length; i++) {
+   if($("#parent span").text() === currentTime) {
+       $("#parent .input").addClass("present");
+   }
+   else if ($("#parent span").text() < currentTime) {
+       $("#parent .input").addClass("past");
+   }
+   else ($("#parent span").text() > currentTime) 
+      { $("#parent .input").addClass("future");
+    }
+   
+   };
 
 
